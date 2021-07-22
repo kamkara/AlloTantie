f79381426f4c43c68dd717e3d29ccd9b5d519478
kamkara <lnclass.kamkara@gmail.com>
2021-07-22 11:49:42 +0000

    add allo charte on dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
+  root to:'home#index'
+  
   resources :countries
   resources :products
   resources :categories
   get 'admin/index'
-  root to:'home#index'
-
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
